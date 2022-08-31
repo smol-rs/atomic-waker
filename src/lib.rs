d7e234ff3a04e20bb1f6eae42e4c66383b336a72
jtnunley <jtnunley01@gmail.com>
2022-08-31 08:56:47 -0700

    fix issues + build for no_std

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 //!
 //! # Features
 //!
-//! This crate adds a feature, `atomic-polyfill`, which uses a polyfill
-//! from the [`atomic-polyfill`] crate in order to provide functionality
-//! to targets without atomics. See the [`README`] for the [`atomic-polyfill`]
-//! crate for more information on how this is implemented by the end-user.
+//! This crate adds a feature, `portable-atomic`, which uses a polyfill
+//! from the [`portable-atomic`] crate in order to provide functionality
+//! to targets without atomics. See the [`README`] for the [`portable-atomic`]
+//! crate for more information on how to use it on single-threaded targets.
 //!
-//! [`atomic-polyfill`]: https://crates.io/crates/atomic-polyfill
-//! [`README`]: https://github.com/embassy-rs/atomic-polyfill/blob/main/README.md
+//! [`portable-atomic`]: https://crates.io/crates/portable-atomic
+//! [`README`]: https://github.com/taiki-e/portable-atomic/blob/main/README.md#optional-cfg
 
 #![no_std]
 
